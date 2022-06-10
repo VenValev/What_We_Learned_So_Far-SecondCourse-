09a05589addcd719fdb8267f148ba5a903c427dd
Vencislav <vencislav67@hotmail.com>
2022-06-10 12:41:30 +0300

    4

---
+++ 04_Print_Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_Print_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start_Seq = int.Parse(Console.ReadLine());
+            int end_Seq = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = start_Seq; i <= end_Seq; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
