8ac785310a7d05c4111a757f963fe21b549b050a
Vencislav <vencislav67@hotmail.com>
2022-06-10 12:49:47 +0300

    11

---
+++ 11_Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11_Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfOrders = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+            while (numberOfOrders > 0)
+            {
+                double priceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int brCapsule = int.Parse(Console.ReadLine());
+                double sum = priceCapsule * days * brCapsule;
+                Console.WriteLine($"The price for the coffee is: ${sum:f2}");
+                totalSum += sum;
+                numberOfOrders--;
+            }
+            Console.WriteLine($"Total: ${totalSum:f2}");
+
+        }
+    }
+}
