80294be508ec02c9c6cf67f43a2d5eac905fbcf3
Vencislav <vencislav67@hotmail.com>
2022-06-10 12:43:32 +0300

    5

---
+++ 05_Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05_Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_Name = Console.ReadLine();
+
+            string pasword = string.Empty;
+
+            for (int i = user_Name.Length - 1; i >= 0; i--)
+            {
+                pasword += user_Name[i];
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                string inputp = Console.ReadLine();
+                if (inputp == pasword)
+                {
+                    Console.WriteLine($"User {user_Name} logged in.");
+                    break;
+                }
+                else if (i != 3)
+                {
+                    Console.WriteLine($"Incorrect password. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"User {user_Name} blocked!");
+                }
+            }
+
+        }
+    }
+}
