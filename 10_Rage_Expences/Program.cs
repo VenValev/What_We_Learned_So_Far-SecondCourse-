46649c6d7c08ca9983b0e4f45632b9f6c91ffa41
Vencislav <vencislav67@hotmail.com>
2022-06-10 12:49:07 +0300

    10

---
+++ 10_Rage_Expences/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10_Rage_Expences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGame = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int brHeadset = lostGame / 2;
+            int brMouse = lostGame / 3;
+            int brKeyboard = lostGame / 6;
+            int brDisplay = lostGame / 12;
+
+            double sum = brHeadset * headsetPrice + brMouse * mousePrice + brKeyboard * keyboardPrice + brDisplay * displayPrice;
+            Console.WriteLine($"Rage expenses: {sum:f2} lv.");
+
+        }
+    }
+}
